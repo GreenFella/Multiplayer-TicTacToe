96e40910471ff1927896c533d7152b528b1b8f2f
Kai <kay.ritti@gmail.com>
2023-02-11 05:26:20 +0000

    Dev1

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-test
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const WebSocket = require('ws');
+
+    app.set('view engine', 'ejs')
+    app.use(methodOverride('_method'))
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
+
+    app.get('/',(req, res) => {
+            res.render('main.ejs')
+      });
+
+    app.listen(80, function() {
+        console.log('Listening on port 80!')
+      })
